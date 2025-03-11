6566e255fa89e7725c905fd3a4de592335a4d01a
kadirdolek <kadirdolek@outlook.com>
2025-03-11 09:46:46 +0100

    ajout des questions/réponses

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,31 @@
-console.log("test")
+
+let questions1 = ["Qui a signé l'armistice mettant fin à la Première Guerre mondiale ?"]
+let questions2 = ["Quel événement a marqué le début de la Seconde Guerre mondiale ?"]
+let questions3 = ["Quel homme est à l'origine de la fin de l'apartheid en Afrique du Sud ?"]
+let questions4 = ["Quel est le nom du président américain pendant la crise des missiles de Cuba ?"]
+let questions5 = ["Quelle mouvement allemand a été un des principaux acteur de la Guerre mondiale(en un mot) ?"]
+let questions6 = ["Quel traité a mis fin à la Première Guerre mondiale ?"]
+let questions7 = ["Quel événement a eu lieu le 11 septembre 2001 ?"]
+let questions8 = ["Qui a été le leader de l'Union soviétique pendant la Seconde Guerre mondiale ?"]
+let questions9 =  ["Dans quel pays a eu lieu la révolution de 1917 ?"]
+let questions10 = ["Quel mur a symbolisé la division de l'Allemagne pendant la guerre froide ?"]
+
+let reponse1 =["Foch"]
+let reponse2 =["Invasion"]
+let reponse3 =["Nelson Mandela"]
+let reponse4 =["Kennedy"]
+let reponse5 =["Nazi"]
+let reponse6 =["Versailles"]
+let reponse7 =["Attentat"]
+let reponse8 =["Staline"]
+let reponse9 =["Russie"]
+let reponse10 =["Berlin"]
+
+
+
+
+console.log("Bonjour, le test qui va suivre sera composé de 20 questions.")
+
+
+
 
